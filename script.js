d05bb4edb8d4eee52c0b49adcc37850e70e66934
sadatrohina <rohinarohiii@gmail.com>
2024-10-22 13:15:28 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var  registerlink = document.querySelector('registerlink');
+var  btn = document.querySelector('btn');
+var  rememberforgot = document.querySelector('rememberforgot');
+var  wrapper = documeert.querySelector('wrapper');
+ 
+registerlink .addEventListener('click',function (){
+    wrapper.classlist.toggle('active');
+    
+})
+ 
+
+rememberforgot.addEventListener('click',()=>{
+    wrapper.classlist.toggle('active');
+})
